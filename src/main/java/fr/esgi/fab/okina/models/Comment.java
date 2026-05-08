50c94aee4e142e268a3edb435f6a17887b940204
Pietot <102545552+Pietot@users.noreply.github.com>
2026-05-08 21:31:48 +0200

    feat(models): add Comment model

---
+++ src/main/java/fr/esgi/fab/okina/models/Comment.java
@@ -0,0 +1,18 @@
+package fr.esgi.fab.okina.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    private String id;
+    private String taskId;
+    private String authorId;
+    private String content;
+    private LocalDateTime createdAt;
+}
